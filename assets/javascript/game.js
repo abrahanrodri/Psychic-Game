dddf6b2700a664e657d4e92e913624f96eed9426
Abe <abrahan.rodri@gmail.com>
2019-06-27 22:28:50 -0400

    homework w/ javascript no errors but no reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,24 +2,40 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var wins = 0;
 var losses = 0;
 var guesses = 10;
+var userTracker = [];  
+var computerChoice = letters[Math.floor(Math.random() * letters.length)];
 
-var computerChoice = alphabet[Math.floor(Math.random() * letters.length)];
+console.log("Wins" + wins)
+console.log("Losses" + losses)
+console.log("Guesses:" + guesses)
+console.log("Computer chose: " + computerChoice)
 
-console.log(computerChoice)
 
 document.onkeypress = function(event) {
-    var userGuess = event.key;
+    var userGuess = event.key.toLowerCase();
+    
+    
 
     if(userGuess === computerChoice){
         wins++;
+        
     }
     else{
         guesses--;
+        
     }
-    if(guesses === 0){
-        losses++
-    }  
+    if(guesses < 0){
+        losses++;  
+        
+    }
+
+    
+    
+    
+    userTracker.push(userGuess);
+
     document.getElementById("wins").innerHTML = "Wins: " + wins;
     document.getElementById("losses").innerHTML = "Losses: " + losses;
-    document.getElementById("guesses").innerHTML = "Guesses left:" + guesses;
-}
+    document.getElementById("guesses").innerHTML = "Guesses left: " + guesses;
+    document.getElementById("userTracker").innerHTML = "Guesses so far: " + userTracker;
+};
